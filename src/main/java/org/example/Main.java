1ffc4a07af6adb2195019ea9f4fa951d89d91e8c
ishutin_va <ishutin_va@outlook.com>
2022-09-04 00:39:47 +0300

    Test deploy 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/", (request, response) -> {
-            return "Hello world5";
+            return "Hello world6";
         });
     }
 }
